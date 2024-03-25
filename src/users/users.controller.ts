c03b9fc073d2b1e5030773bedb057d98d6eb4f12
Guilherme <guiteixeira1926@gmail.com>
2024-03-25 09:19:38 -0400

    feat: Add users controller methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignUpDTO } from './dto/signUp.dto';
+import { User } from './models/user.model';
+import { SignInDTO } from './dto/signIn.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signUp(@Body() signUp: SignUpDTO): Promise<User> {
+    return this.usersService.signUp(signUp);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async signIn(
+    @Body() signIn: SignInDTO,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    return this.usersService.signIn(signIn);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
